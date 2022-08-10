2413db29f2bb0655bb48b271a0224689af294eb7
labanshiks <labanshiks4@gmail.com>
2022-08-10 23:58:28 +0300

    second commit

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://labanshiks:sHiks254@cluster0.ymputwj.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
