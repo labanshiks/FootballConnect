1c9f8ac7587436745b054fe08109d7a59bf05359
labanshiks <labanshiks4@gmail.com>
2022-08-12 01:25:14 +0300

    fourth commit

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
-import Post from './Post/Post';
 
+import Post from './Post/Post';
 import useStyles from './styles';
 
 
@@ -11,12 +12,15 @@ const Posts = () => {
 
     console.log(posts)
     return (
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-        </>
-
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6} md={6}>
+                        <Post post={post} />
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 };
 
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://labanshiks:sHiks254@cluster0.ymputwj.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://labanshiks:sHiks254@cluster0.ymputwj.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
