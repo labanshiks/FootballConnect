fcd2aef19f4e50b91a935a6c25a459f8e918cf45
labanshiks <labanshiks4@gmail.com>
2022-08-15 23:58:22 +0300

    fifth commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
-
-import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
+import memories from './images/memories.png';
 
-function App() {
-
+const App = () => {
+  const [currentId, setCurrentId] = useState(null);
   const dispatch = useDispatch();
   const classes = useStyles();
 
@@ -31,10 +30,10 @@ function App() {
         <Container>
           <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import PostMessage from '../models/postMessage.js';
 
@@ -28,4 +29,14 @@ export const createPost = async (req, res) => {
     }
 }
 
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send(`No post with that id`);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+    res.json(updatedPost);
+}
 export default router;
