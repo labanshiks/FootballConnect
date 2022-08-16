8e4292185f7f2c852c1801ab5c75da58c4d995e1
labanshiks <labanshiks4@gmail.com>
2022-08-16 23:18:10 +0300

    sixth commit

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,9 @@ const App = () => {
   const dispatch = useDispatch();
   const classes = useStyles();
 
-
   useEffect(() => {
     dispatch(getPosts());
-  }, [dispatch]);
-
-
+  }, [currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
@@ -28,7 +25,7 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+          <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} />
             </Grid>
--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -3,13 +3,10 @@ import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
 
-
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
-
 const Form = ({ currentId, setCurrentId }) => {
-
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     const post = useSelector((state) => (currentId ? state.posts.find((p) => p._id === currentId) : null));
     const classes = useStyles();
@@ -19,10 +16,9 @@ const Form = ({ currentId, setCurrentId }) => {
         if (post) setPostData(post);
     }, [post]);
 
-
-
     const clear = () => {
-
+        setCurrentId(null);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
     };
 
     const handleSubmit = (e) => {
@@ -33,13 +29,14 @@ const Form = ({ currentId, setCurrentId }) => {
         } else {
             dispatch(createPost(postData));
         }
+        clear();
     };
 
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant="h6">Creating a Memory</Typography>
+                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
                 <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
                 <TextField name="message" variant="outlined" label="Message" fullWidth multiline minRows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
