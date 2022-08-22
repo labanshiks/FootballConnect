ed34ab043ab3cbe8d50dc81d40ea7414b4c5b41f
labanshiks <labanshiks4@gmail.com>
2022-08-22 22:51:47 +0300

    twelfth commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import PostDetails from './components/PostDetails/PostDetails';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Auth from './components/Auth/Auth';
 
-const App = () => (
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'));
 
-  <BrowserRouter>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/auth" exact element={<Auth />} />
-      </Routes>
-    </Container>
-  </BrowserRouter>
-);
+  return (
+    <BrowserRouter>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Navigate to="/posts" />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" exact element={<PostDetails />} />
+          <Route path="/auth" exact element={(!user ? <Auth /> : <Navigate to="/posts" />)} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
+};
 
 export default App;
