01bfbb71780799d4989ad6b36f231641e03ffb43
labanshiks <labanshiks4@gmail.com>
2022-09-11 20:24:30 +0300

    updating client

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -64,9 +64,16 @@ const Post = ({ post, setCurrentId }) => {
                     <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
                 </div>
                 {(userId === post?.creator) && (
-                    <div className={classes.overlay2}>
-                        <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}>
-                            <MoreHorizIcon fontSize="medium" />
+                    <div className={classes.overlay2} name="edit">
+                        <Button
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setCurrentId(post._id);
+                            }}
+                            style={{ color: 'white' }}
+                            size="small"
+                        >
+                            <MoreHorizIcon fontSize="default" />
                         </Button>
                     </div>
                 )}
