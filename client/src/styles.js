2510dad8086a2943277dce14db93e5c2e6248bd7
labanshiks <labanshiks4@gmail.com>
2022-09-12 23:44:35 +0300

    Updated Navbar

--- client/src/styles.js
+++ client/src/styles.js
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
     appBar: {
-        borderRadius: 15,
         margin: '30px 0',
         display: 'flex',
         flexDirection: 'row',
