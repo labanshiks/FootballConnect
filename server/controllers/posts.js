4492e7bef80e4617dc327dcc2003cc55e36becd1
labanshiks <labanshiks4@gmail.com>
2022-09-14 22:43:58 +0300

    Update client side

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -11,7 +11,7 @@ export const getPosts = async (req, res) => {
     const { page } = req.query;
 
     try {
-        const LIMIT = 8;
+        const LIMIT = 6;
         const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
 
         const total = await PostMessage.countDocuments({});
